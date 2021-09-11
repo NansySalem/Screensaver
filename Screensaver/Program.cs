8cf62b885318954c2e5fcb8c016dee9a6fb3c7f1
Nansy Salem <v-nansysalem@microsoft.com>
2021-09-10 19:20:35 -0700

    Adding comments to main function

--- Screensaver/Program.cs
+++ Screensaver/Program.cs
@@ -7,6 +7,7 @@ namespace Screensaver
     {
         static void Main(string[] args)
         {
+            //Array where random quote will be selected for the screen saver
             String[] quotes = new string[]
             {
                "Hard work pays off.",
@@ -21,10 +22,15 @@ namespace Screensaver
                "Never give up.",
                "Even the world impossible says I'm possible."
             };
-            String text = "Hello World!";
-        //AnimationUtils.animate(text, 100);
+          
+          String text = "Hello World!";
 
-        AnimationUtils.animate(quotes, 50, 300);
+            //From below, please run one line at a time to see the desired screensaver option
+            //Uncommenting the line below runs the first animate function displaying "Hello world"
+            //AnimationUtils.animate(text, 100);
+
+            //Uncommenting the line below runs the second animate function displaying random quotes
+            AnimationUtils.animate(quotes, 50, 300);
 
         }
     }
